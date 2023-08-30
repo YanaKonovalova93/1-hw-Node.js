85a1f081a3daf77f46e200fda2e18da5285a1eea
YanaKonovalova93 <118968627+YanaKonovalova93@users.noreply.github.com>
2023-08-30 16:47:10 +0300

    finish project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 1-hw-Node.js
+
+https://monosnap.com/list/64ef4719bd159e094f3fea65 
--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log('Hi, iana')
+import { program } from "commander";
+
+import contactsService from "./contacts.js";
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contactsService.listContacts();
+      console.log(allContacts);
+      break;
+
+    case "get":
+      const oneContact = await contactsService.getContactById(id);
+      console.log(oneContact);
+      break;
+    case "add":
+      const newContact = await contactsService.addContact({
+        name,
+        email,
+        phone,
+      });
+      console.log(newContact);
+      break;
+    case "remove":
+      const removeContact = await contactsService.removeContact(id);
+      console.log(removeContact);
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+program
+  .option("-a, --action <type>")
+  .option("-i, --id <type>")
+  .option("-n, --name <type>")
+  .option("-p, --phone <type>")
+  .option("-e, --email <type>");
+
+program.parse();
+
+const options = program.opts();
+invokeAction(options);
